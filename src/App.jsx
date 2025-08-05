a8c93f2032a8a56e41cf08735eaf841f7846ad8f
VarshiniPrudhvi09 <varshiniprudhvi09@gmail.com>
2025-08-05 15:07:29 +0530

    json-file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,75 @@
-import React, { createContext, useState } from 'react'
-import ChildComponent from './Component/ChildComponent';
 
-let MessageContext = createContext();
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  let [message, setMessage] = useState("Hello this is message");
-  let [coins, setCoins] = useState([1,2,3,4,5,6]) ;
+
+  let [notes, setNotes] = useState([]) ;
+
+ let [newName, setNewName] = useState([]);
+ let [newGender, setNewGender] = useState([]) ;
+
+  console.log(notes);
+  let fetchData = async() =>{
+    await axios.get('http://localhost:3001/note')
+              
+              .then((response) => setNotes(response.data));
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  let handleForm = async(e)=>{
+    e.preventDefault();
+
+    let newObject = {
+      id:notes.length + 1,
+      name:newName,
+      gender:newGender
+    };
+
+    await axios.post('http://localhost:3001/note',newObject)
+          .then(()=>alert("new data added success"));
+
+          fetchData();
+          setNewName('')
+          setNewGender("")
+  }
 
   return (
-   <MessageContext.Provider value={{message, coins}}>
-      <ChildComponent/>
-   </MessageContext.Provider>
+    <div>
+        <h3>List of Notes</h3>
+        <div>
+          {
+           notes && notes.map((note)=>{
+            return (
+              <div>
+                  <p>Name:{note.name}</p>
+                  <p>Gender:{note.gender}</p>
+                  <hr></hr>
+              </div>
+            )
+           })
+             
+          }
+        </div>
+        <form onSubmit={handleForm}>
+          <input type='text' 
+            placeholder='Enter name'
+            onChange={(e)=>setNewName(e.target.value)}
+            value={newName}
+          /><br/>
+          <input
+            type="text"
+            placeholder='Enter gender'
+            onChange={(e)=>setNewGender(e.target.value)}
+            value={newGender}
+          /><br/>
+          <button type='submit'>submit</button>
+        </form>
+    </div>
   )
 }
 
-export {App as default, MessageContext} ;
+export default App ;
