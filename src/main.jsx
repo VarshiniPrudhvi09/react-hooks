a448f7217222a842793222fa04575d4512a73317
VarshiniPrudhvi09 <varshiniprudhvi09@gmail.com>
2025-08-06 14:16:54 +0530

    react redux initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,41 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-let notes = [
-    {
-        id:1,
-        content:"web applications use js",
-        important:true
-    },
-    {
-        id:2,
-        content:"web application 2",
-        important:true
-    },
-    {
-        id:3,
-        content:"web applications 3",
-        important:true
-    },
-    {
-        id:4,
-        content:"web applications use 4",
-        important:true
-    },
+import { createStore } from 'redux'
 
-];
+let noteReducer = (state=0, action)=>{
+    switch(action.type){
+        case "Incr":
+            return state + 1;
+        case "Decr":
+            return state - 1;
+    }
+
+}
+const store = createStore(noteReducer) ;
+
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+store.dispatch({
+    type:'Incr'
+})
+
+store.dispatch({
+    type:'Incr'
+})
+
+store.dispatch({
+    type:'Incr'
+})
+
+
+
+store.dispatch({
+    type:"Decr"
+})
+
 createRoot(document.getElementById('root')).render(
   
-    <App note = {notes} />
+    <App />
  
 )
